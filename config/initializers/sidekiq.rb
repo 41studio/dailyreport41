b597631b38b05663a9f033f0191b9a60854fbb3d
Avid Maulana Saputra <avid@41studio.com>
2017-02-03 16:43:04 +0700

    Remove redis namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,10 @@ require 'sidekiq'
 
 unless Rails.env.development?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"], namespace: "dailyreport41_#{Rails.env}" }
+    config.redis = { url: ENV["REDIS_URL"] }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"], namespace: "dailyreport41_#{Rails.env}" }
+    config.redis = { url: ENV["REDIS_URL"] }
   end
 end
