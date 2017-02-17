d44593693142abf8e0b18c2b769f75771fda8566
Avid Maulana Saputra <avid@41studio.com>
2017-02-17 13:32:22 +0100

    Fix render report template

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,7 +11,7 @@ class ReportsController < ApplicationController
   # GET /reports/1
   # GET /reports/1.json
   def show
-    @body_template = ReportTemplate.new(@report).render
+   @body_template = @report.template
   end
 
   # GET /reports/new
