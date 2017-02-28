76d9d3010c180b870bec6cf548e3e78663ac6deb
Avid Maulana Saputra <avid@41studio.com>
2017-02-28 11:02:06 +0100

    Fix filter recaps

--- app/controllers/recaps_controller.rb
+++ app/controllers/recaps_controller.rb
@@ -6,18 +6,17 @@ class RecapsController < ApplicationController
   # GET /recaps
   # GET /recaps.json
   def index
-    @projects = Project.joins(:user).where.not(last_updated: nil).select("projects.id, projects.name, projects.last_updated, projects.user_id, users.full_name AS user_name").order(last_updated: :desc)
-    @projects =
+    recap_range =
       if params.key?(:start_date) and params.key?(:end_date)
-        @projects.where(last_updated: params[:start_date].concat('T00:00:00')..params[:end_date].concat('T23:59:59'))
+        date_to_range(params[:start_date], params[:end_date])
       else
         current_date = Time.zone.now
-        recap_range  = current_date.beginning_of_week.strftime("%Y%m%d").concat('T00:00:00')..current_date.end_of_week.strftime("%Y%m%d").concat('T23:59:59')
-        @projects.where(last_updated: recap_range)
+        date_to_range(current_date.beginning_of_week.strftime("%Y%m%d"), current_date.end_of_week.strftime("%Y%m%d"))
       end
 
-    @projects = @projects.where("projects.name ILIKE ?", "%#{params[:project]}%") if params[:project].present?
-    @projects = @projects.where("users.full_name ILIKE ?", "%#{params[:reported_by]}%") if params[:reported_by].present?
+    @projects = Project.joins(user: :reports).where.not(last_updated: nil).where(reports: {reported_at: recap_range}).select("projects.id, projects.name, projects.last_updated, projects.user_id, users.full_name AS user_name").order(last_updated: :desc).group("projects.id, projects.name, projects.last_updated, projects.user_id, users.full_name")
+    @projects = @projects.where("projects.name ILIKE ?",   "%#{params[:project]}%")       if params[:project].present?
+    @projects = @projects.where("users.full_name ILIKE ?", "%#{params[:reported_by]}%")   if params[:reported_by].present?
     @projects = @projects.page(params[:page]).per(20)
   end
 
@@ -76,30 +75,18 @@ class RecapsController < ApplicationController
   end
 
   def view
-    @project = Project.find_by(id: params[:project_id])
-    @reports = Report.filter_by(params)
+    @results = {}
+    project = Project.find_by(id: params[:project_id])
+    @results[:project_name] = project.try(:name)
+    @results[:project_user_name] = project.try(:user).try(:full_name)
+    @results[:reports] = Report.filter_by(params)
 
     start_date = Date.parse(params[:start_date]).strftime("%B #{Date.parse(params[:start_date]).day.ordinalize}, %Y") rescue nil
     end_date = Date.parse(params[:end_date]).strftime("%B #{Date.parse(params[:end_date]).day.ordinalize}, %Y") rescue nil
-    @date_range = "#{start_date} - #{end_date}"
-    @title_recap = "[#{@project.name}] #{@date_range} - #{@project.user.try(:full_name)}"
-    respond_to do |format|
-      format.html {render layout: false}
-      format.pdf do
-        pdf = WickedPdf.new.pdf_from_url(view_recaps_url(pdf: true), {
-          orientation: 'Landscape',
-          page_size: 'A4',
-          margin: {
-            top:    10,
-            bottom: 10,
-            left:   10,
-            right:  10
-          }
-        })
-
-        send_data(pdf, filename: "#{@title_recap}.pdf", type: "application/pdf")
-      end
-    end
+    @results[:date_range] = "#{start_date} - #{end_date}"
+    @results[:title_recap] = "[#{@results[:project_name]}] #{@results[:date_range]} - #{@results[:project_user_name]}"
+    @results[:total_work_hour] = @results[:reports].map(&:work_hour).sum
+    respond_to_pdf("#{@results[:title_recap]}.pdf")
   end
 
   private
@@ -111,5 +98,29 @@ class RecapsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def recap_params
       params.require(:recap).permit(:project_id, :user_id, :start_date, :end_date)
+    end
+
+    def date_to_range(start_date, end_date)
+      start_date.concat('T00:00:00')..end_date.concat('T23:59:59')
+    end
+
+    def respond_to_pdf(filename)
+      respond_to do |format|
+        format.html {render layout: false}
+        format.pdf do
+          pdf = WickedPdf.new.pdf_from_url(view_recaps_url(pdf: true), {
+            orientation: 'Landscape',
+            page_size: 'A4',
+            margin: {
+              top:    10,
+              bottom: 10,
+              left:   10,
+              right:  10
+            }
+          })
+
+          send_data(pdf, filename: filename, type: "application/pdf")
+        end
+      end
     end
 end
