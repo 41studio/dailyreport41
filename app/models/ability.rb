a739aea675d4a19b1ebea479ea29cf2816d69cc8
Avid Maulana Saputra <avid@41studio.com>
2017-03-06 07:56:32 +0100

    Add permission for role bde

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.ceo?
       can :manage, :all
-    elsif user.manager?
+    elsif user.manager? || user.bde?
       can :manage, Recap
     else
       can :read, :all
