a00bbc9f6a913feb1b5a96340cf823eddd6d5ec1
Avid Maulana Saputra <avid@41studio.com>
2017-03-13 11:51:43 +0100

    Update redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,10 @@ require 'sidekiq'
 
 unless Rails.env.development?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"] }
+    config.redis = { url: ENV["REDISCLOUD_URL"] }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"] }
+    config.redis = { url: ENV["REDISCLOUD_URL"] }
   end
 end
