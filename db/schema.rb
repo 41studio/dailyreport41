c6f41968813b2f7e592a5a8cc6f2672f9403d871
Radiet <radiet@41studio.com>
2017-04-07 18:30:41 +0700

    Change mailer configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308073121) do
+ActiveRecord::Schema.define(version: 20170310024707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 20170308073121) do
     t.string   "message_id"
     t.boolean  "resend",      default: false
     t.string   "slug"
-    t.decimal  "work_hour",   default: 8.0
+    t.float    "work_hour",   default: 8.0
     t.text     "end_letter",  default: "If you have any question, feel free to contact me directly and I'll try my best to help.<br><br>Have a wonderful day ;)"
   end
 
